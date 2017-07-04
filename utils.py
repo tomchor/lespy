a78bc8b7c03b15c18e33573cc65455b0f7401386
tomchor <tomaschor@gmail.com>
2017-07-03 19:33:37 -0700

    included a newer version of f90nml

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 def paramParser(nmlpath):
     """Function that parses parameters from param.nml namelist files
     """
-    from .nml import read
+    #from .nml import read
+    from .f90nml import read
     from os.path import basename, isfile, exists, join, abspath
     from os import remove
 
-    buffername = nmlpath.replace('/', '_') + '.buffer'
     if exists(nmlpath):
         #----------
         # If nmlpath is a file, it's probably just the param.nml file
@@ -27,19 +27,8 @@ def paramParser(nmlpath):
     else:
         raise ValueError('Path {} doesnt exist'.format(nmlpath))
 
-    #----------
-    # lines with % are not supported (type character resolver)
-    # rls_src has variables in it, so python can't understand it
-    nml = namelist.readlines()
-    nml = [ line for line in nml if '%' not in line ]
-    nml = [ line for line in nml if 'rls_src' not in line ]
-    aux = open(buffername, 'wt')
-    aux.writelines(nml)
-    aux.close()
-    #----------
 
-    groups = read(buffername)
-    remove(buffername)
+    groups = read(namelist)
     params = {}
     for key in groups.keys():
         params.update(groups[key])
@@ -460,4 +449,53 @@ cdict2 = {'red':   ((0.0, 0.0, 0.0),
 
 blue_red1 = _lin_cmap('BlueRed1', cdict1)
 blue_red2 = _lin_cmap('BlueRed2', cdict2)
+
+def _paramParser(nmlpath):
+    """Function that parses parameters from param.nml namelist files
+    """
+    from .nml import read
+    #from .f90nml import read
+    from os.path import basename, isfile, exists, join, abspath
+    from os import remove
+
+    buffername = nmlpath.replace('/', '_') + '.buffer'
+    if exists(nmlpath):
+        #----------
+        # If nmlpath is a file, it's probably just the param.nml file
+        if isfile(nmlpath):
+            namelist=open(nmlpath, 'rt')
+        #----------
+
+        #----------
+        # If it's a dir, we first look the param.nml in the dir and then in the codebkp dir
+        else:
+            try:
+                namelist = open(join(nmlpath, 'param.nml'), 'rt')
+                print('Found param.nml at {}'.format(abspath('param.nml')))
+            except:
+                namelist = open(join(nmlpath, 'codebkp/param.nml'), 'rt')
+                print('Found param.nml at {}'.format(abspath('codebkp/param.nml')))
+        #----------
+
+    else:
+        raise ValueError('Path {} doesnt exist'.format(nmlpath))
+
+    #----------
+    # lines with % are not supported (type character resolver)
+    # rls_src has variables in it, so python can't understand it
+    nml = namelist.readlines()
+    nml = [ line for line in nml if '%' not in line ]
+    nml = [ line for line in nml if 'rls_src' not in line ]
+    aux = open(buffername, 'wt')
+    aux.writelines(nml)
+    aux.close()
+    #----------
+
+    groups = read(buffername)
+    remove(buffername)
+    params = {}
+    for key in groups.keys():
+        params.update(groups[key])
+    return params
+
 
